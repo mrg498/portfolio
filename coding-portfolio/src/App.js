9a311ce0583ea9f343eac6c790e054aa511b2ad9
Miles Grossenbacher <mrg498@nyu.edu>
2020-09-04 16:33:23 -0400

    restarting this

--- coding-portfolio/src/App.js
+++ coding-portfolio/src/App.js
@@ -1,17 +1,11 @@
 import React, { Component } from 'react';
 import './App.css';
-import Routes from './components/Routes';
-import Navigation from './components/Navigation';
-import Footer from './components/Footer';
-
 
 class App extends Component {
 	render() {
 		return (
 			<div className="App">
-				<Navigation />
-				<Routes/>
-				<Footer/>
+				<div className="App_gradient"></div>
 			</div>
 		);
 	}

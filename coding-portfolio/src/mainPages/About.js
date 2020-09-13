0e279283c668fddd016bf089406efa4ae3bed497
Miles Grossenbacher <mrg498@nyu.edu>
2020-09-13 18:07:30 -0400

    resume and cv page content and styling

--- coding-portfolio/src/mainPages/About.js
+++ coding-portfolio/src/mainPages/About.js
@@ -1,18 +1,19 @@
 import React, { Component } from 'react';
 import Jumbotron from '../components/Jumbotron';
 import Footer from '../components/Footer';
+import Headshot from '../images/headshot.jpeg';
 import './About.css';
 
 export default class About extends Component {
 	render() {
 		return (
-			<div className="About">
+			<div className="About Page">
 				<div className="container">
 					<Jumbotron pageName="Miles Grossenbacher" />
 				</div>
 				<main>
 					<section className="container">
-						<p className="About__text">
+						<p className="page-info__text">
 							Welcome to my portfolio! I am a sotware engineer and web developer and a recent graduate of
 							NYU with majors in Computer Science and Audio Technology. I've worked extensively in
 							Javascript and Java, but I've also completed projects in C++, Python, and Swift. As a full
@@ -21,9 +22,15 @@ export default class About extends Component {
 							using React. My backend experience includes working professionally in the Spring MVC Java
 							framework, and building projects in Node.js. Aside from web applications, I've built a
 							multitude of other projects in a variety of different langauges from Audio Plugins in C++ to
-							iPhone games in swift. But don't just take my word for it. Check out my projects to see what
+							iPhone games in Swift. But don't just take my word for it. Check out my projects to see what
 							I can do!
 						</p>
+                        <div className="row justify-content-center">
+                            <div className="col-12 col-md-6 image-col">
+								<div style={{backgroundImage: `url(${Headshot})`, backgroundSize:'contain', backgroundRepeat:'no-repeat', height: '20vh', padding:'0px'}}>
+								</div>
+                            </div>
+                        </div>
 					</section>
 				</main>
 				<Footer />
--- coding-portfolio/src/mainPages/Resume.js
+++ coding-portfolio/src/mainPages/Resume.js
@@ -1,15 +1,47 @@
 import React, { Component } from 'react';
 import Jumbotron from '../components/Jumbotron';
 import Footer from '../components/Footer';
+import pdf from '../images/Summer2020Resume.pdf';
 import './Resume.css';
 
 export default class Resume extends Component {
 	render() {
 		return (
-			<div className="Resume">
+			<div className="Resume Page">
 				<div className="container">
 					<Jumbotron pageName="CV & Resume" />
 				</div>
+				<main>
+					<section className="container">
+						<h3 className="section-header">My Resume:</h3>
+						<iframe title="pdf" src={pdf} width="100%" height="800px" className="pdf__iframe" />
+						<p className="pdf__link">
+							Open my <a href={pdf}>resume</a> as a PDF file.
+						</p>
+					</section>
+					<section className="container">
+						<h3 className="section-header">My CV:</h3>
+						<h5 className="section-header__subheader">Education</h5>
+						<p className="cv-text">
+							<ul>
+								<li>NYU Computer Science</li>
+								<li>NYU Music Technology</li>
+								<li>
+									IRCAM (Institute for Research and Coordination in Acoustics/Music), Paris, Fr. Dec.
+									2016 Studies in Digital Signal Processing and Sound Spatialization with MAX/MSP
+								</li>
+							</ul>
+						</p>
+						<h5 className="section-header__subheader">Work Experience</h5>
+						<p className="cv-text">
+							<ul>
+								<li>Handoff</li>
+								<li>Massive Music</li>
+								<li>NYU Clive Davis</li>
+							</ul>
+						</p>
+					</section>
+				</main>
 				<Footer />
 			</div>
 		);

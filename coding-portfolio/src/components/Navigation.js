87d66d050da7dc9e65e35561bfe35a6c0daf783f
Miles Grossenbacher <mrg498@nyu.edu>
2020-10-18 22:05:34 -0400

    fixed the production routing a smarter way

--- coding-portfolio/src/components/Navigation.js
+++ coding-portfolio/src/components/Navigation.js
@@ -7,9 +7,9 @@ export default class Navigation extends Component {
         return (
             <nav className="Navigation">
                 <ul className='nav-links'>
-                    <li className="nav-link"><NavLink activeClassName="active-link" to={process.env.PUBLIC_URL + '/about'}>About Me</NavLink></li>
-                    <li className="nav-link"><NavLink activeClassName="active-link" to={process.env.PUBLIC_URL + '/projects'}>Projects</NavLink></li>
-                    <li className="nav-link"><NavLink activeClassName="active-link" to={process.env.PUBLIC_URL + '/resume'}>Resume/CV</NavLink></li>      
+                    <li className="nav-link"><NavLink activeClassName="active-link" to={'/about'}>About Me</NavLink></li>
+                    <li className="nav-link"><NavLink activeClassName="active-link" to={'/projects'}>Projects</NavLink></li>
+                    <li className="nav-link"><NavLink activeClassName="active-link" to={'/resume'}>Resume/CV</NavLink></li>      
                 </ul> 
             </nav>
         )
